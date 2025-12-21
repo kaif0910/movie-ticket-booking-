f1694ecde51572b614e56e2dcce00d6b1cfb787e
kaif0910 <mdkaifperwez@gmail.com>
2025-12-21 18:05:24 +0530

    creating movie model

---
+++ models/movie.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const movieSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required : true
+    },
+    description :{
+        type: String,
+        required : true
+    },
+    casts:{
+        type:[String],
+        required : true
+    },
+    trailerUrl:{
+        type: String,
+        required : true
+    },
+    language:{
+        type:String,
+        required : true,
+        default: "English"
+    },
+    releaseDate:{
+        type:String,
+        required : true
+    },
+    releaseStatus:{
+        type:String,
+        required : true,
+        default:"Released"
+    },
+},{timestamps:true});
+
+const Movie = mongoose.model("Movie",movieSchema);//creates a new model
+
+module.exports = Movie;//returning the model
