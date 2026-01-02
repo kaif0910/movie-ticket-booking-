2da1360a87ebd5cecd2425c17852b40a3674cdf4
kaif0910 <mdkaifperwez@gmail.com>
2026-01-02 11:57:53 +0530

    fix: fixed the user signup route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const userController = require("../controllers/user.controller");
 
 const routes = (app) =>{
     app.post(
-        "/mba/api/v1/createUser",
+        "/mba/api/v1/auth/signup",
         userController.createUser
     );
 }
