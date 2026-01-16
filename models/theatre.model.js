cc8a25d0e615a690f6fdbca2a3ecf96c2237cdc4
kaif0910 <mdkaifperwez@gmail.com>
2026-01-16 14:47:15 +0530

    feat: added theatre owner property to theatre model

--- models/theatre.model.js
+++ models/theatre.model.js
@@ -19,6 +19,11 @@ const theatreSchema =new mongoose.Schema({
         type: String,
         required: true
     },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
     movies:{
         type: [mongoose.Schema.Types.ObjectId],
         ref: "Movie"
